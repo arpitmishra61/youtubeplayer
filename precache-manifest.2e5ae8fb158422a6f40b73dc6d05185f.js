1d03d22c0235592baf03dea2320a9efcbaac2c35
Arpit Mishra <arpitmishra61@gmail.com>
2020-01-11 22:21:20 +0530

    Updates

---
+++ precache-manifest.2e5ae8fb158422a6f40b73dc6d05185f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb055f871f5b96be45dd596bbbd12ebc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b917d6807ac7b51aa9a",
+    "url": "/static/css/main.432994c5.chunk.css"
+  },
+  {
+    "revision": "3d51e1062580448426d1",
+    "url": "/static/js/2.e0f7bfaa.chunk.js"
+  },
+  {
+    "revision": "adf315857185901d18c2339ec58b6b66",
+    "url": "/static/js/2.e0f7bfaa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4b917d6807ac7b51aa9a",
+    "url": "/static/js/main.fdab9be5.chunk.js"
+  },
+  {
+    "revision": "ac33c1599890b5b68704",
+    "url": "/static/js/runtime-main.129c9baf.js"
+  }
+]);
